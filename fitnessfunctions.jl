d9ce692d4861ac83a83f15aa8809df85107c2f89
Pablo Duque <menduke@live.com>
2018-10-30 17:25:39 +0100

    Completed fitness functs and function for evaluating all entities in a population

--- fitnessfunctions.jl
+++ fitnessfunctions.jl
@@ -1,12 +1,34 @@
 
-function sphere(variables)
-    sum = 0
-    for element in variables
-        sum += element^2
+function evaluate_all_fitness!(entities, fitness_func, eval_on="phenotype")
+
+    if eval_on == "phenotype"
+        for entity in entities
+            entity.fitness = fitness_func(entity.phenotype)
+        end
+
+    elseif eval_on == "genotype"
+        for entity in entities
+            entity.fitness = fitness_func(entity.genotype)
+        end
     end
-    return sum
+
+end
+
+
+function sphere(variables)
+    _sum = 0
+    for element in variables
+        _sum += element^2
+    end
+    return _sum
 end
 
 function schwefel(variables)
-
+    len_vars = length(variables)
+    _sum = 0
+    for (index, element) in enumerate(variables)
+        _sum += element * sin(abs(element)^(1/2))
+    end
+    schwefel_const = 418.9829
+    fitness = schwefel_const * len_vars - _sum
 end
