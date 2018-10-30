fe36976c94f3e298b669e63c07270275287bb1bb
Pablo Duque <menduke@live.com>
2018-10-30 17:26:23 +0100

    Started base code for full algorithim

---
+++ generic_algo.jl
@@ -0,0 +1,13 @@
+
+num_generations = 100
+population = []
+fitness_function = func
+parent_selection =
+
+for gen in range(num_generations)
+
+    fitness_values = fitness_function(population)
+    population[]
+end
+
+sorted = sort(population, by=i->i.fitness)
