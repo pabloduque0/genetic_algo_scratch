2972d12f2a6b0b052eadec04b9a3bdbc5028f288
Pablo Duque <menduke@live.com>
2018-11-04 20:14:44 +0100

    Auto stash before merge of "master" and "origin/master"

--- elements.jl
+++ elements.jl
@@ -5,21 +5,19 @@ mutable struct Entity
     n_genes
     phenotype
     genotype
-    fitness
 end
 
 
-function generate_population(length, number_genes, variable_range=(0, 100))
+function generate_population(length, number_genes, range=(0, 100))
     population = []
     for elem in range(0, 98)
-        if variable_range[1] > 0
+        if range[0] > 0
             real_range_1 = rand(Float16, 1)[1] * 100
             real_range_2 = rand(Float16, 1)[1] * 100
         else
-            real_range_1 = (rand(Float16, 1)[1] * (variable_range[1]*2)) - variable_range[1]
-            real_range_2 = (rand(Float16, 1)[1] * (variable_range[2]*2)) - variable_range[2]
+            real_range_1 = (rand(Float16, 1)[1] * (range[0]*2)) - range[0]
+            real_range_2 = (rand(Float16, 1)[1] * (range[1]*2)) - range[1]
         end
-
 
         if real_range_1 >= real_range_2
             full_range = (real_range_2, real_range_1)
@@ -29,7 +27,7 @@ function generate_population(length, number_genes, variable_range=(0, 100))
             phenotypes, genotypes = generate_variables(real_range_1, real_range_2, number_genes)
         end
 
-        entity = Entity(full_range, number_genes, phenotypes, genotypes, nothing)
+        entity = Entity(full_range, number_genes, phenotypes, genotypes)
         push!(population, entity)
     end
     return population
@@ -42,9 +40,9 @@ function generate_variables(real_range_start, real_range_stop, steps)
     for element in range(0, steps)
         binary_string = get_string_genotype(steps)
         int_number = parse(Int, binary_string, base = 2)
-        step_size = (real_range_stop - real_range_start) / (steps - 1)
+        step_size = real_range_stop - real_range_start
 
-        phenotype = real_range_start + (step_size * int_number)
+        phenotype = real_range_start + (step_size*int_number)
         push!(genotypes, binary_string)
         push!(phenotypes, phenotype)
     end
@@ -57,14 +55,6 @@ function get_string_genotype(length)
     binary_array = convert(Array{Int, 1}, bit_array_string)
     binary_string = join(binary_array, "")
     return binary_string
-end
-
-function assign_all_fitness!(population, func)
-
-    for entity in population
-        fitness_val = func(entity.phenotype)
-        entity.fitness = fitness_val
-    end
 end
 
 
