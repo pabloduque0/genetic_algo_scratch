e7f87facdba687a695b732d034ae46eb051fa5b9
Pablo Duque <menduke@live.com>
2018-11-06 22:08:37 +0100

    Last version of the algorithim

--- generic_algo.jl
+++ generic_algo.jl
@@ -2,10 +2,10 @@
 num_generations = 100
 population = []
 fitness_function = func
-parent_selection = 
+parent_selection =
 
 for gen in range(num_generations)
     evaluate_all_fitness!(population, fitness_func, eval_on="phenotype")
-    sorted_populations = sort(population, by=i->i.fitness, rev=true)
-
+    parents = selection_by_rank(population)
+    two_point_xover(parents)
 end
