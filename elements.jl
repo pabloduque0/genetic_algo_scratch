bc9be04a077214ab1ddd6e788ab01d0e982a82f6
Pablo Duque <menduke@live.com>
2018-11-11 20:19:40 +0100

    Updated and refactored elements file

--- elements.jl
+++ elements.jl
@@ -5,18 +5,20 @@ mutable struct Entity
     n_genes
     phenotype
     genotype
+    fitness
+    age
 end
 
 
 function generate_population(length, number_genes, range=(0, 100))
     population = []
-    for elem in range(0, 98)
-        if range[0] > 0
+    for elem = 0:98
+        if range[1] > 0
             real_range_1 = rand(Float16, 1)[1] * 100
             real_range_2 = rand(Float16, 1)[1] * 100
         else
-            real_range_1 = (rand(Float16, 1)[1] * (range[0]*2)) - range[0]
-            real_range_2 = (rand(Float16, 1)[1] * (range[1]*2)) - range[1]
+            real_range_1 = (rand(Float16, 1)[1] * (range[1]*2)) - range[1]
+            real_range_2 = (rand(Float16, 1)[1] * (range[2]*2)) - range[2]
         end
 
         if real_range_1 >= real_range_2
@@ -27,7 +29,12 @@ function generate_population(length, number_genes, range=(0, 100))
             phenotypes, genotypes = generate_variables(real_range_1, real_range_2, number_genes)
         end
 
-        entity = Entity(full_range, number_genes, phenotypes, genotypes)
+        entity = Entity(full_range,
+                        number_genes,
+                        phenotypes,
+                        genotypes,
+                        nothing,
+                        1)
         push!(population, entity)
     end
     return population
@@ -37,7 +44,7 @@ function generate_variables(real_range_start, real_range_stop, steps)
 
     phenotypes = []
     genotypes = []
-    for element in range(0, steps)
+    for element = 0:steps
         binary_string = get_string_genotype(steps)
         int_number = parse(Int, binary_string, base = 2)
         step_size = real_range_stop - real_range_start
