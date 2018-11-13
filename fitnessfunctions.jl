2a7782bb7c5970b0fb8e17f4b0bf0249e3302cf4
Pablo Duque <menduke@live.com>
2018-11-14 00:01:47 +0100

    Added module sintax and indentation. Minor refactoring

--- fitnessfunctions.jl
+++ fitnessfunctions.jl
@@ -1,34 +1,36 @@
+module FitnessFunctions
 
-function evaluate_all_fitness!(entities, fitness_func, eval_on="phenotype")
+    function evaluate_all_fitness!(entities, fitness_func, eval_on="phenotype")
 
-    if eval_on == "phenotype"
-        for entity in entities
-            entity.fitness = fitness_func(entity.phenotype)
+        if eval_on == "phenotype"
+            for entity in entities
+                entity.fitness = fitness_func(entity.phenotype)
+            end
+
+        elseif eval_on == "genotype"
+            for entity in entities
+                entity.fitness = fitness_func(entity.genotype)
+            end
         end
 
-    elseif eval_on == "genotype"
-        for entity in entities
-            entity.fitness = fitness_func(entity.genotype)
+    end
+    
+    function sphere(variables)
+        _sum = 0
+        for element in variables
+            _sum += element^2
         end
+        return _sum
     end
 
-end
-
-
-function sphere(variables)
-    _sum = 0
-    for element in variables
-        _sum += element^2
+    function schwefel(variables)
+        len_vars = length(variables)
+        _sum = 0
+        for (index, element) in enumerate(variables)
+            _sum += element * sin(abs(element)^(1/2))
+        end
+        schwefel_const = 418.9829
+        fitness = schwefel_const * len_vars - _sum
     end
-    return _sum
-end
 
-function schwefel(variables)
-    len_vars = length(variables)
-    _sum = 0
-    for (index, element) in enumerate(variables)
-        _sum += element * sin(abs(element)^(1/2))
-    end
-    schwefel_const = 418.9829
-    fitness = schwefel_const * len_vars - _sum
-end
+end  # module FitnessFunctions
