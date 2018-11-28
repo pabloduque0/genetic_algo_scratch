f89a197151848f4c793020e704322675b0b7304f
Pablo Duque <menduke@live.com>
2018-11-28 12:44:07 +0100

    Added algo params from utils. Removed prints

--- generic_algo.jl
+++ generic_algo.jl
@@ -4,24 +4,21 @@ include("./mutation.jl")
 include("./parent_selections.jl")
 include("./population.jl")
 include("./survival_selection.jl")
-
-using .Crossover, .FitnessFunctions, .Mutation, .ParentSelection, .Population, .SurvivalSelection
+include("./utils.jl")
+using .Crossover, .FitnessFunctions, .Mutation, .ParentSelection, .Population, .SurvivalSelection, .Utils
 using Statistics, Plots
 
-num_generations = 100
 
-population_size = 100
-number_genes = 64
-variable_range = (-10, 10)
+num_generations,  population_size, number_genes, variable_range = Utils.get_algo_params()
+
 population = Population.generate_population(population_size, number_genes, variable_range)
-fitness_function = FitnessFunctions.sphere
+fitness_function = FitnessFunctions.schwefel
 
-fitness_evaluation = []
+fitness_evaluation_mean = []
+fitness_evaluation_best = []
 for gen in 1:num_generations
-    #println("Initial population: ", population[1])
     FitnessFunctions.evaluate_all_fitness!(population, fitness_function, "phenotype")
     parents = ParentSelection.selection_by_rank(population)
-    #println("Mid state: ", parents[1])
     offspring = Crossover.generate_offspring(parents, Crossover.two_point_xover)
     Mutation.perform_all_mutations!(offspring, Mutation.binary_mutation)
 
@@ -29,11 +26,11 @@ for gen in 1:num_generations
     offspring = push!(offspring, fittest)
     deleteat!(offspring, 1)
     global population = offspring
-    #println("Final: ", population[1])
     fitness_values = [e.fitness for e in population]
-    #println(fitness_evaluation)
-    push!(fitness_evaluation, mean(fitness_values))
+    push!(fitness_evaluation_mean, mean(fitness_values))
+    push!(fitness_evaluation_best, fittest.fitness)
 
 end
-#print(fitness_evaluation)
-#plot(1:num_generations, fitness_evaluation)
+
+plot(1:num_generations, fitness_evaluation_mean, lw=3)
+plot!(1:num_generations, fitness_evaluation_best, lw=3)
