e80c7addd7d9c42cf275deafca89d6f904ccf16f
Pablo Duque <menduke@live.com>
2018-12-12 22:19:08 +0100

    Started grey code based on some python code to do so

--- utils.jl
+++ utils.jl
@@ -1,6 +1,6 @@
 module Utils
     function get_algo_params()
-        file_info = open("/harddrive/home/pablo/Google Drive/UNED/Computacion_Evolutiva/Actividades/Actividad_1/genetic_algo_scratch/algo_inputs.txt") do file
+        file_info = open("/home/cx02392/Documentos/Personal/UNED/Computacion_Evolutiva/Actividad1/genetic_algo_scratch/algo_inputs.txt") do file
             read(file, String)
         end
         file_info = split(file_info, "\n")
@@ -31,12 +31,58 @@ module Utils
         return num_generations,  population_size, number_genes, variable_range
     end
 
-    function gray_code_recursive()
 
+"""
+def gray_code(n):
+    def gray_code_recurse (g,n):
+        k=len(g)
+        if n<=0:
+            return
 
+        else:
+            for i in range (k-1,-1,-1):
+                char='1'+g[i]
+                g.append(char)
+            for i in range (k-1,-1,-1):
+                g[i]='0'+g[i]
+
+            gray_code_recurse (g,n-1)
+
+    g=['0','1']
+    gray_code_recurse(g,n-1)
+    return g
+
+def main():
+    n=int(raw_input())
+    g = gray_code (n)
+
+    if n>=1:
+        for i in range (len(g)):
+            print g[i],
+
+"""
+
+    function gray_code_recursive(binary_string, n)
+
+        length_str = length(binary_string)
+
+        if n <= 0
+            return
+        elseif
+            for i in length_str:-1:1
+                char = '1' * binary_string[i]
+                binary_string.append(char)
+            end
+            for i in length_str:-1:1
+                binary_string[i] = '0' + binary_string[i]
+            end
+            gray_code_recursive(binary_string,n-1)
+        end
     end
 
-    function generate_graycode()
+    function generate_graycode(binary_string)
+
+
 
     end
 end
