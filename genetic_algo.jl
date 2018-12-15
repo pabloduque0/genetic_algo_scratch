f4f24fc15885cb3b1e17c379119a1c50706c12a2
Pablo Duque <menduke@live.com>
2018-12-15 21:12:41 +0100

    Renamed file and minor changes

--- genetic_algo.jl
+++ genetic_algo.jl
@@ -8,7 +8,6 @@ include("./utils.jl")
 using .Crossover, .FitnessFunctions, .Mutation, .ParentSelection, .Population, .SurvivalSelection, .Utils
 using Statistics, Plots
 
-
 num_generations,  population_size, number_genes, variable_range = Utils.get_algo_params()
 
 population = Population.generate_population(population_size, number_genes,
@@ -17,12 +16,13 @@ fitness_function = FitnessFunctions.schwefel
 
 fitness_evaluation_mean = []
 fitness_evaluation_best = []
+mutations_levels = []
 for gen in 1:num_generations
     FitnessFunctions.evaluate_all_fitness!(population, fitness_function, "phenotype")
     parents = ParentSelection.selection_by_rank(population)
     offspring = Crossover.generate_offspring(parents, Crossover.two_point_xover)
-    Mutation.perform_all_mutations!(offspring, Mutation.binary_mutation)
-
+    mutation_level = Mutation.perform_all_mutations!(offspring, Mutation.binary_mutation)
+    push!(mutations_levels, mean(mutation_level))
     fittest = SurvivalSelection.select_single_best(population)
     offspring = push!(offspring, fittest)
     deleteat!(offspring, 1)
@@ -34,4 +34,6 @@ for gen in 1:num_generations
 end
 
 plot!(1:num_generations, [fitness_evaluation_mean, fitness_evaluation_best], lw=3, layout=(1, 2))
-#plot!(1:num_generations, fitness_evaluation_best, lw=3)
+#plot(1:num_generations, mutations_levels, lw=3)
+
+#savefig("/harddrive/home/pablo/Google Drive/UNED/Computacion_Evolutiva/Actividades/Actividad_1/genetic_algo_scratch/fitness_mean_schewel.png")
